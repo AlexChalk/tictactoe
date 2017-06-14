809bd0cdeb463e87e124b57624a38e079a0df0d9
Alex Chalk <alexchalk17@gmail.com>
2017-06-14 11:35:50 -0400

    Refactor square design

--- src/square.js
+++ src/square.js
@@ -2,28 +2,37 @@
   'use strict';
   var square = function() {
     var mark = '';
+    var incorrectMarking = {
+      name: 'Error',
+      message: 'A square can only be marked with an "X" or "0"'
+    };
+    var marksCannotBeOverwritten = {
+      name: 'Error',
+      message: 'A square cannot be marked more than once'
+    };
+    var insertMarkIfXOr0 = function(marking) {
+      if (marking === 'X' || marking === '0') {
+        mark = marking;
+      } else {
+        throw incorrectMarking;
+      }
+    };
+
     return {
+
       getMark: function() {
         return mark;
       },
+
       setMark: function(marking) {
         if (mark === '') {
-          if (marking === 'X' || marking === '0') {
-            mark = marking;
-          } else {
-            throw {
-              name: 'Error',
-              message: 'A square can only be marked with an "X" or "0"'
-            };
-          }
+          insertMarkIfXOr0(marking);
         } else {
-          throw {
-            name: 'Error',
-            message: 'A square cannot be marked more than once'
-          };
+          throw marksCannotBeOverwritten;
         }
         return this;
       }
+
     };
   };
   exports.square = square;
