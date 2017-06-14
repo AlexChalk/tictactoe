1873394b22d909429dc72c014feee48f66c55525
Alex Chalk <alexchalk17@gmail.com>
2017-06-14 13:15:16 -0400

    Test that board alternates between 'X' and '0' when marking squares

--- src/board.js
+++ src/board.js
@@ -3,26 +3,26 @@
   var board = function() {
     var turn = 0;
     var squares = {
-      'x0y0': square(),
-      'x0y1': square(),
-      'x0y2': square(),
-      'x1y0': square(),
-      'x1y1': square(),
-      'x1y2': square(),
-      'x2y0': square(),
-      'x2y1': square(),
-      'x2y2': square(),
+      x0y0: square(),
+      x0y1: square(),
+      x0y2: square(),
+      x1y0: square(),
+      x1y1: square(),
+      x1y2: square(),
+      x2y0: square(),
+      x2y1: square(),
+      x2y2: square(),
     };
     var updatedSquares = {};
+
     return {
       markSquare: function(square) {
         if (turn % 2 === 0) {
           squares[square].setMark('X');
-          updatedSquares[square] = 'X';
         } else {
           squares[square].setMark('0');
-          updatedSquares[square] = 'Y';
         }
+        updatedSquares[square] = squares[square].getMark();
         turn += 1;
       },
       getUpdatedSquares: function() {
--- test/boardSpec.js
+++ test/boardSpec.js
@@ -11,8 +11,18 @@ describe('board', function() {
     expect(myBoard.getUpdatedSquares()).to.eql({ x0y0: 'X' });
   });
 
+  it('alternates between "X" and "0" when marking squares', function() {
+    myBoard.markSquare('x0y0'); myBoard.markSquare('x0y1'); myBoard.markSquare('x0y2'); myBoard.markSquare('x1y0');
+    expect(myBoard.getUpdatedSquares()).to.eql({ 
+      x0y0: 'X',
+      x0y1: '0',
+      x0y2: 'X',
+      x1y0: '0'
+    });
+  });
+
   xit('has mocking abilities', function() {
-    sinon.spy(console, 'log');
+    sinon.stub(console, 'log');
     console.log('hey');
     expect(console.log).to.have.been.calledOnce;
   });
