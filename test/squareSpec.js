3d1597b17feb885ed00e426f3ee1ce63f68dab17
Alex Chalk <alexchalk17@gmail.com>
2017-06-14 11:40:42 -0400

    Confirm with test that square's 'mark' var is not directly accessible

--- test/squareSpec.js
+++ test/squareSpec.js
@@ -5,15 +5,23 @@ describe('square', function() {
   beforeEach(function() {
     square0 = square();
   });
+
+  it('is initialized with mark variable that is not directly accessible', function() {
+    expect(square0.mark).to.not.equal('');
+  });
+
   it('can be marked', function() {
     square0.setMark('X');
     expect(square0.getMark()).to.equal('X');
   });
+
   it('can only be marked with an X or 0', function() {
     expect(square0.setMark.bind(square0, 'A')).to.throw(/can only be marked with an "X" or "0"/);
   });
+
   it('cannot be marked more than once', function() {
     square0.setMark('X');
     expect(square0.setMark.bind(square0, '0')).to.throw(/cannot be marked more than once/);
   });
+
 });
